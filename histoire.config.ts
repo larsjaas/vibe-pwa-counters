156243a10bd97afdd861ca74aa6a1dc3f20a33ea
Lars J. Aas <lars.aas@eyasys.no>
2026-04-25 13:43:07 +0200

    Attempt to get Histoire working

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'histoire'
 
 export default defineConfig({
-  // History configuration
+  storyMatch: ['src/**/*.story.@(ts|tsx|js|jsx)'],
+  server: {
+    host: '0.0.0.0',
+  },
 })
